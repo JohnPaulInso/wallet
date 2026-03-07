7fdaea0106363e9e116b991dd364ec49e87e36ab
JohnPaulInso <88543202+JohnPaulInso@users.noreply.github.com>
2026-03-07 12:40:28 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,60 +1,30 @@
-// Firebase Configuration and Initialization
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithRedirect, getRedirectResult, signOut, onAuthStateChanged, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, collection, addDoc, getDoc, getDocs, query, orderBy, where, doc, setDoc, updateDoc, deleteDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getAuth, signInAnonymously, GoogleAuthProvider, signInWithPopup, signInWithRedirect, getRedirectResult, onAuthStateChanged, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, doc, getDoc, setDoc, addDoc, collection, serverTimestamp, getDocs, onSnapshot, query, orderBy, updateDoc, deleteDoc, deleteField, enableIndexedDbPersistence, writeBatch } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Firebase configuration
+// FIREBASE CONFIG
 const firebaseConfig = {
-  apiKey: "AIzaSyBHDN0xLi98qjYYjUf2RDCn5gJK0dzTk_M",
-  authDomain: "atome-wallet.firebaseapp.com",
-  projectId: "atome-wallet",
-  storageBucket: "atome-wallet.firebasestorage.app",
-  messagingSenderId: "64186651619",
-  appId: "1:64186651619:web:cd7d426cfb663aab6606d6",
-  measurementId: "G-2SHWJ6S3Z3"
+    apiKey: "AIzaSyBHDN0xLi98qjYYjUf2RDCn5gJK0dzTk_M",
+    authDomain: "atome-wallet.firebaseapp.com",
+    projectId: "atome-wallet",
+    storageBucket: "atome-wallet.firebasestorage.app",
+    messagingSenderId: "64186651619",
+    appId: "1:64186651619:web:cd7d426cfb663aab6606d6",
+    measurementId: "G-2SHWJ6S3Z3"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
-// Explicitly set persistence
+// Pre-set persistence
 setPersistence(auth, browserLocalPersistence).catch(e => console.error("Persistence error", e));
 
+// Export core instances
+export { app, auth, db };
 
-// Gmail API Configuration
-const GMAIL_API_KEY = firebaseConfig.apiKey;
-const GMAIL_CLIENT_ID = "64186651619-3eb9ki680f4c8q2g2mese3c8hhfur23b.apps.googleusercontent.com";
-const GMAIL_SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
-
-// Export for use in other files
-export {
-    app,
-    auth,
-    db,
-    analytics,
-    GoogleAuthProvider,
-    signInWithPopup,
-    signInWithRedirect,
-    getRedirectResult,
-    signOut,
-    onAuthStateChanged,
-    collection,
-    addDoc,
-    getDoc,
-    getDocs,
-    query,
-    orderBy,
-    where,
-    doc,
-    setDoc,
-    updateDoc,
-    deleteDoc,
-    serverTimestamp,
-    GMAIL_API_KEY,
-    GMAIL_CLIENT_ID,
-    GMAIL_SCOPES
+// Re-export Firebase methods for convenience in other modules
+export { 
+    doc, getDoc, setDoc, addDoc, collection, serverTimestamp, getDocs, onSnapshot, query, orderBy, updateDoc, deleteDoc, deleteField, enableIndexedDbPersistence, writeBatch,
+    signInAnonymously, GoogleAuthProvider, signInWithPopup, signInWithRedirect, getRedirectResult, onAuthStateChanged
 };

15b925a08e2a927f558a9328899c120757d9824f
John Paul Inso <john.paulinso@fusionos.ai>
2026-08-21 13:34:50 +0800

    Update

--- a.bat
+++ a.bat
@@ -1,3 +1,12 @@
 @echo off
-REM Ultra-quick command: just type "a" to build and open Android Studio
-call scripts\dev.bat
+rem (2026-07-13) Compile standalone APK and open output, prev open Studio
+call npm run sync-www
+call npx cap sync android
+call npm run fix-java
+cd /d "%~dp0android"
+call gradlew.bat assembleDebug
+cd /d "%~dp0"
+if exist "%~dp0android\app\build\outputs\apk\debug" (
+    explorer "%~dp0android\app\build\outputs\apk\debug"
+)
+exit /b 0
